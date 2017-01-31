44d417ffdc00a0d86df76f4bae8d3120e3f70713
ic2000 <ic2000@live.co.uk>
2017-01-31 21:52:47 +0000

    Update main.c

--- main.c
+++ main.c
@@ -9,6 +9,8 @@
 #define SCR_W 80
 #define SCR_H 25
 
+#define B_SIZE 32
+
 typedef char scr_t[SCR_H][SCR_W + 1];
 
 typedef struct {
@@ -38,24 +40,46 @@ void print_scr(scr_t scr) {
 }
 
 void append_scr(scr_t scr, line_t line) {
+    if (line.p1.x < 0 || line.p1.x > SCR_W) {
+        printf("error: invalid arg '%d'; out of x range (0 - %d)\n",
+            line.p1.x, SCR_W);
+        exit(1);
+    }
+
+    if (line.p2.x< 0 || line.p2.x > SCR_W) {
+        printf("error: invalid arg '%d'; out of x range (0 - %d)\n",
+            line.p2.x, SCR_W);
+        exit(1);
+    }
+
+    if (line.p1.y < 0 || line.p1.y > SCR_H) {
+        printf("error: invalid arg '%d'; out of x range (0 - %d)\n",
+            line.p2.x, SCR_H);
+        exit(1);
+    }
+
+    if (line.p2.y < 0 || line.p2.y > SCR_H) {
+        printf("error: invalid arg '%d'; out of x range (0 - %d)\n",
+            line.p2.x, SCR_H);
+        exit(1);
+    }
+
     for (int x = line.p1.x; x <= line.p2.x; x++) {
         int y = line.p1.y + line.pd.y * (x - line.p1.x) / line.pd.x;
         scr[y][x] = '*';
     }
 }
 
-void raise_error(char* msg, int status) {
-    printf("error: %s\n", msg);
-    exit(status);
-}
-
-
 char* get_line(FILE* fptr, line_t* lptr) {
-    static char buff[32];
-    char* str = fgets(buff, 32, fptr);
+    static char line[B_SIZE];
+    static char buff[B_SIZE];
+
+    char* str = fgets(line, B_SIZE, fptr);
     if (!str) {
         return str;
     }
+    
+    strcpy(buff, line);
 
     char* bptr = buff;
     char* tok;
@@ -66,30 +90,32 @@ char* get_line(FILE* fptr, line_t* lptr) {
 
         int n = atoi(tok);
         if (n == 0 && tok[0] != '0') {
-            raise_error("invalid value(s)", 1);
+            printf("error: invalid arg '%s'; expected type 'int'\n", tok);
+            exit(1);
         }
 
-        *(int*[4]) {
-            &lptr->p1.x, &lptr->p1.y,
-                &lptr->p2.x, &lptr->p2.y
-        }[i - 1] = n;
+        *(int*[4]) { &lptr->p1.x, &lptr->p1.y,
+                &lptr->p2.x, &lptr->p2.y }[i - 1] = n;
     }
 
     if (i < 5) {
-        raise_error("missing argument(s)", 1);
+        printf("error: invalid line '%s'; missing arg(s)\n", line);
+        exit(1);
     }
 
     return str;
 }
 
 int main(int argc, char** argv) {
-    if (!argv[1]) {
-        printf("Usage:\n    linegen <path-to-file>\n");
+    if (!argv[1] || strcmp(argv[1], "help") == 0) {
+        printf("usage:\n    linegen <path-to-file>\n\n");
+        exit(1);
     }
 
     FILE* fptr = fopen(argv[1], "r");
     if (!fptr) {
-        raise_error("file not found", 1);
+        printf("error: file not found\n");
+        exit(1);
     }
 
     scr_t scr;
@@ -101,15 +127,16 @@ int main(int argc, char** argv) {
         line.pd.y = line.p2.y - line.p1.y;
 
         if (line.pd.x == 0 || line.pd.y == 0) {
-            raise_error("invalid line(s)", 1);
+            printf("error: invalid line '%d %d %d %d'; difference amounts to 0\n",
+                line.p1.x, line.p1.y, line.p2.x, line.p2.y);
+            exit(1);
         }
 
         append_scr(scr, line);
     }
 
-    fclose(fptr);
-
     print_scr(scr);
 
+    fclose(fptr);
     return 0;
 }
