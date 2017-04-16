d88965322d9d2c3797766ca4351d13e84bfa6630
ic2000 <ic2000@live.co.uk>
2017-04-16 19:25:59 +0100

    a
    a

--- main.c
+++ main.c
@@ -14,15 +14,15 @@ int main(int argc, char **argv) {
     Scr *scr = scr_new(75, 25);
 
     Lines *lines = lines_from_file(
-    	"lines.txt",
-    	point_new(scr->width, scr->height)
+        argv[1],
+        point_new(scr->width, scr->height)
     );
 
     for (size_t l = 0; l < lines->len; l++) {
-    	Points *line_points = points_from_line(lines->data[l]);
-    	scr_draw_points(scr, line_points, '*');
+        Points *line_points = points_from_line(lines->data[l]);
+        scr_draw_points(scr, line_points, '*');
 
-    	free(line_points);
+        free(line_points);
     }
 
     scr_print(scr);
